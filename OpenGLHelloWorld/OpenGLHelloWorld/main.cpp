9be9732fbf775b951043c5d84290c4d16246aff6
Vayne-Lover <406378362@qq.com>
2016-06-26 12:39:04 +0800

    OpenGLHelloWorld

---
+++ OpenGLHelloWorld/OpenGLHelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLHelloWorld
+//
+//  Created by 姜况童 on 2016/6/26.
+//  Copyright © 2016年 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
