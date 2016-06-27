cf50300883fefee096ce6a3e17c9df2171e94b70
Vayne-Lover <406378362@qq.com>
2016-06-27 09:59:03 +0800

    OpenGL HelloWorld

--- OpenGLHelloWorld/OpenGLHelloWorld/main.cpp
+++ OpenGLHelloWorld/OpenGLHelloWorld/main.cpp
@@ -6,10 +6,24 @@
 //  Copyright © 2016年 Vayne-Lover. All rights reserved.
 //
 
-#include <iostream>
+#include <GLUT/glut.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex2d(-0.5, -0.5);
+    glVertex2d(-0.5, 0.5);
+    glVertex2d(0.5, 0.5);
+    glVertex2d(0.5, -0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char ** argv) {
+    
+    glutInit(&argc, argv);//Deprecated
+    glutCreateWindow("OpenGL Start Example");
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
