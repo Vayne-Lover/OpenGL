d96f26ac18953c4a9f22c32c71cb190512b760af
Vayne-Lover <406378362@qq.com>
2016-07-04 10:26:12 +0800

    Exercise0

--- OpenGLExercise0/OpenGLExercise0/main.cpp
+++ OpenGLExercise0/OpenGLExercise0/main.cpp
@@ -7,9 +7,14 @@
 //
 
 #include <iostream>
+#include<GLUT/GLUT.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    enum VAO_IDs{Triangles,NumVAOs};
+    enum Buffer_IDs{ArrayBuffer,NumBuffers};
+    enum Attrib_IDs{vPosition=0};
+    GLuint VAOs[NumVAOs];
+    GLuint Buffers[NumBuffers];
+    const GLuint NumVertices=6;
     return 0;
 }
